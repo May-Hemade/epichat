f820d2cf7f534461e8d712956cb8127d303c9157
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-03-28 13:08:22 +0300

    server

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import cors from "cors"
+
+import passport from "passport"
+import {
+    badRequestHandler,
+    unauthorizedHandler,
+    notFoundHandler,
+    genericErrorHandler,
+    forbiddenHandler,
+    catchAllHandler,
+} from "./errorHandlers.js"
+import mongoose from "mongoose"
+
+
+const server = express()
+
+const port = process.env.PORT || 3002
+// passport.use("google", googleStrategy)
+
+/************************************** Middleware **************************/
+server.use(cors());
+server.use(express.json());
+server.use(passport.initialize())
+
+
+/************************************** Enpoints **************************/
+
+
+
+
+server.use(express.json())
+
+
+// console.table(listEndpoints(server))
+// server.use(badRequestHandler)
+// server.use(unauthorizedHandler)
+// server.use(notFoundHandler)
+// server.use(genericErrorHandler)
+// server.use(forbiddenHandler)
+// server.use(catchAllHandler)
+
+
+
+export { server }
