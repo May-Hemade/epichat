54f9c337d68bef48e6d90d0969517118fb6fb06e
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-03-29 16:23:39 +0300

    fix port

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,6 @@ import passport from "passport";
 import usersRouter from './services/users'
 import googleStrategy from './services/auth/oauth';
 
-
-
-
 const server = express()
 const port = process.env.PORT || 3001
 process.env.TS_NODE_DEV && require("dotenv").config()
@@ -18,21 +15,17 @@ server.use(express.json());
 server.use(cors())
 server.use(passport.initialize())
 
-
-
-
 server.use('/users', usersRouter)
 
+if (!process.env.MONGO_CONNECTION) {
+  throw Error("Url is undefined!")
+}
+mongoose.connect(process.env.MONGO_CONNECTION)
 
-if(!process.env.MONGO_CONNECTION){
-    throw Error("Url is undefined!")
-  }
-  mongoose.connect(process.env.MONGO_CONNECTION)
-  
-  mongoose.connection.on("connected", () => {
-    console.log("Successfully connected to Mongo!")
-    server.listen(3001, () => {
-      console.table(listEndpoints(server))
-      console.log("Server runnning on port: ", 3001)
-    })
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!")
+  server.listen(port, () => {
+    console.table(listEndpoints(server))
+    console.log("Server runnning on port: ", 3001)
   })
+})
